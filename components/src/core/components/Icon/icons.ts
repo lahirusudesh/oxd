92d9945afeafe424ec39dbe3471ac4696c2d491b
infas <infas@orangehrm.com>
2026-06-08 11:27:39 +0530

    Added twitter X icon

--- components/src/core/components/Icon/icons.ts
+++ components/src/core/components/Icon/icons.ts
@@ -653,6 +653,12 @@ export const oxdTwitter: icon = {
     '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path style="fill:#03A9F4" fill="#03A9F4" d="M16 3.539a6.839 6.839 0 0 1-1.89.518 3.262 3.262 0 0 0 1.443-1.813 6.555 6.555 0 0 1-2.08.794 3.28 3.28 0 0 0-5.674 2.243c0 .26.022.51.076.748a9.284 9.284 0 0 1-6.761-3.431 3.285 3.285 0 0 0 1.008 4.384A3.24 3.24 0 0 1 .64 6.578v.036a3.295 3.295 0 0 0 2.628 3.223 3.274 3.274 0 0 1-.86.108 2.9 2.9 0 0 1-.621-.056 3.311 3.311 0 0 0 3.065 2.285 6.59 6.59 0 0 1-4.067 1.399c-.269 0-.527-.012-.785-.045A9.234 9.234 0 0 0 5.032 15c6.036 0 9.336-5 9.336-9.334 0-.145-.005-.285-.012-.424A6.544 6.544 0 0 0 16 3.539z"/></svg>',
 };
 
+export const oxdTwitterX: icon = {
+  name: 'oxd-twitter-x',
+  value:
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30"><path d="M26.37,26l-8.795-12.822l0.015,0.012L25.52,4h-2.65l-6.46,7.48L11.28,4H4.33l8.211,11.971L12.54,15.97L3.88,26h2.65 l7.182-8.322L19.42,26H26.37z M10.23,6l12.34,18h-2.1L8.12,6H10.23z"/></svg>',
+};
+
 export const oxdLinkedIn: icon = {
   name: 'oxd-linkedin',
   value:
@@ -3114,6 +3120,7 @@ const icons: Icons = {
   'oxd-linkedin': oxdLinkedIn,
   'oxd-facebook': oxdFacebook,
   'oxd-twitter': oxdTwitter,
+  'oxd-twitter-x': oxdTwitterX,
   'oxd-lock': oxdLock,
   'oxd-filereplace': oxdFileReplace,
   'oxd-timezone': oxdTimezone,
